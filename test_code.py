aff930b19a974585a3dbd720446f03bfe4ed466f
Apoorv <apoorvjha18@gmail.com>
2021-07-10 02:29:35 +0530

    Fixed the index out of bound error in the code snippet. Caused due to randint(0,len(companies)) apart from some more indentation issues.

--- test_code.py
+++ test_code.py
@@ -2,23 +2,23 @@ import stockMarketSimulator as sim
 from random import randint
 if __name__=='__main__':
 	n=int(input("Enter number of entities : "))
-    companies=[]
+	companies=[]
 	for i in range(n):
-	    name=input(f"Enter name of #entity[{i+1}] : ")
-		price=float(input("Enter the price of {name}'s share : "))
-		volume=int(input("Enter the volume of share : "))
+		name=input(f"Enter name of #entity[{i+1}] : ")
+		price=float(input(f"Enter the price of {name}'s share : "))
+		volume=int(input(f"Enter the volume of {name}'s share : "))
 		companies.append(sim.Company(name,price,volume))
 	time=int(input("Enter the quantum of simulation time : "))
 	market=sim.StockMarket(companies,time)
 	users=[]
 	n_users=int(input("Enter the number of users : "))
 	for i in range(n_users):
-        seed=float(input("Enter the amount to invest : "))
+		seed=float(input(f"Enter the amount of {i+1} user's investment : "))
 		users.append(market.register(seed))
 	done=market.reset()
 	while(not done):
-	    market.render()
+		market.render()
 		action=market.action_space.sample()
 		id=users[randint(0,len(users)-1)]
-		firm=companies[randint(0,len(companies))].name
+		firm=companies[randint(0,len(companies)-1)].name
 		obs, reward, done, info=market.step(action,id,firm)
