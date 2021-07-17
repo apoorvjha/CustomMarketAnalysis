d2786e7681f73daac65e7029155859b5e20e40f7
Apoorv <apoorvjha18@gmail.com>
2021-07-17 20:08:36 +0530

    some bug fix.

--- RL_Agent.py
+++ RL_Agent.py
@@ -31,7 +31,7 @@ class Experience:
         return (len(self.memory))
 
 class DQN:
-    def __init__(self,input_shape,n_output,mode=0):
+    def __init__(self,input_shape,n_output,mode=1):
         '''
         [+] Mode=0, if the model needs to contain Convolution layers.
         [+] Mode=1, if the model is composed of LSTM layers.
@@ -43,9 +43,9 @@ class DQN:
         '''
         self.model=Sequential()
         if mode==0:
-            self.model.add(Conv1D(16,2,input_shape=input_shape,padding='same',activation='relu'))
-            self.model.add(Conv1D(32,2,padding='same',activation='relu'))
-            self.model.add(Conv1D(64,2,padding='same',activation='relu'))
+            self.model.add(Conv1D(16,1,input_shape=input_shape,padding='same',activation='relu'))
+            self.model.add(Conv1D(32,1,padding='same',activation='relu'))
+            self.model.add(Conv1D(64,1,padding='same',activation='relu'))
             self.model.add(Flatten())
             self.model.add(Dense(input_shape[0] * 64,activation='relu'))
             self.model.add(Dropout(0.2))
@@ -60,10 +60,11 @@ class DQN:
             self.model.add(LSTM(8,dropout=0.2,input_shape=input_shape,return_sequences=True))
             self.model.add(LSTM(16,dropout=0.2,return_sequences=True))
             self.model.add(LSTM(32,dropout=0.2,return_sequences=True))
+            self.model.add(Flatten())
             self.model.add(Dense(16,activation='relu'))
             self.model.add(Dropout(0.2))
             self.model.add(Dense(n_output))
-        self.model.compile(optimizer=Adam(1e-3),loss='huber',metrics=['mse'])
+        self.model.compile(optimizer=Adam(lr=1e-3),loss='huber',metrics=['mse'])
     def getModelInstance(self):
         return self.model
 
@@ -72,13 +73,13 @@ class Agent:
     def __init__(self,input_shape,n_output,id):
         self.id=id
         self.GAMMA=0.999             # The discount factor for normalizing future reward.
-        self.BATCH_SIZE=32           # The chunk of states provisioned randomly for training.
+        self.BATCH_SIZE=128           # The chunk of states provisioned randomly for training.
         # ---------- Epsilon greedy strategy variables -------------#
         self.EPSILON_START=0.9       
         self.EPSILON_END=0.05
         self.EPSILON_DECAY=200
         #-----------------------------------------------------------#
-        self.TARGET_UPDATE=10
+        self.TARGET_UPDATE=20
         self.action_space=n_output
         '''
         The TARGET_VALUE is number of episodes after which the weights and biases of target network
@@ -94,16 +95,17 @@ class Agent:
         self.time_step=0
 
     def selectAction(self,state):
-        EPSILON_THRESHOLD=self.EPSILON_END + (self.EPSILON_START - self.EPSILON_END) * exp(-1 * self.time_step / EPSILON_DECAY)
+        EPSILON_THRESHOLD=self.EPSILON_END + (self.EPSILON_START - self.EPSILON_END) * exp(-1 * self.time_step / self.EPSILON_DECAY)
         self.time_step+=1
         if random() > EPSILON_THRESHOLD:
             # Exploitation
-            return self.policy_net.predict(state)[0]
+            return self.policy_net.predict(state)
         else:
             # Exploration
             return randint(0,self.action_space)
     def optimize(self,episode_number):
         if len(self.memory) < self.BATCH_SIZE:
+            print("Memory instances insuffcient for training!")
             return
         transitions=self.memory.sample(self.BATCH_SIZE)
         current_states=array([transition[0] for transition in transitions])
@@ -120,7 +122,7 @@ class Agent:
                 new_q=Reward
             current_q_value=current_q_values[index]
             current_q_value[Action]=new_q
-            X.append(current_state)
+            X.append(Current_State)
             Y.append(current_q_value)
         self.policy_net.fit(array(X),array(Y),batch_size=self.BATCH_SIZE,verbose=0,shuffle=False)
         if episode_number % self.TARGET_UPDATE == 0:
