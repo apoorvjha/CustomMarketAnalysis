86d5b05e677ec733ee197b25fbfd242b3aa41f01
Apoorv <apoorvjha18@gmail.com>
2021-07-24 02:06:25 +0530

    Changed the code based on the UX requirement

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app.secret_key='QwErTY9934@123'
 @app.route('/')
 def analytics_portal():
 	return render_template('simulator.html')
-@app.route('/analyze')
+@app.route('/about')
+def about():
+	return render_template('about.html')	
+@app.route('/analyze',methods=['POST'])
 def analyze():
-    '''
+	'''
 	In this function, the stock market simulator and trader agents will be initialized.
 	'''
-	pass
+	return "<h1>site under construction</h1>"
