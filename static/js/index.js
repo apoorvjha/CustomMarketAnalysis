3d8dcf3938e3cb3fbe70b5b38d427a8a418b8b43
Apoorv <apoorvjha18@gmail.com>
2021-08-28 19:46:15 +0530

    Added code to dynamically provision input boxes based on user requirements.

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,19 @@
 function loadEntity(){
     var n_entities=document.getElementById('n_entities').value;
-    console.log(n_entities);
+    var i=0;
+    var entity=document.getElementById('entity');
+    for(;i<n_entities;i++){
+        entity.innerHTML+='<label>Name</label><input type="text" name="Name'+i+'"><br>';
+        entity.innerHTML+='<label>Price</label><input type="text" name="Price'+i+'"><br>';
+        entity.innerHTML+='<label>Volume</label><input type="text" name="Volume'+i+'"><br>';
+    }
+}
+
+function loadUsers(){
+    var n_users=document.getElementById('n_users').value;
+    var i=0;
+    var user=document.getElementById('user');
+    for(;i<n_users;i++){
+        user.innerHTML+='<label>Seed</label><input type="number" name="seed'+i+'" required><br>';
+    }
 }
