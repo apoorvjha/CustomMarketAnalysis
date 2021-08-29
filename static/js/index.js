84a23439d36e8941142b09f83a65a7026c168252
Apoorv <apoorvjha18@gmail.com>
2021-08-29 16:59:04 +0530

    Resolved the appearance of dynamically rendered input boxes.

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,11 @@ function loadEntity(){
     var n_entities=document.getElementById('n_entities').value;
     var i=0;
     var entity=document.getElementById('entity');
+    entity.innerHTML='<h3>&nbsp;&nbsp;&nbsp;Entity Information</h3><br><br>';
     for(;i<n_entities;i++){
-        entity.innerHTML+='<label>Name</label><input type="text" name="Name'+i+'"><br>';
-        entity.innerHTML+='<label>Price</label><input type="text" name="Price'+i+'"><br>';
-        entity.innerHTML+='<label>Volume</label><input type="text" name="Volume'+i+'"><br>';
+        entity.innerHTML+='&nbsp;&nbsp;&nbsp;<label>Name</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Name'+i+'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small id="" class="text-muted"><font color="blue">Enter Name of Entity '+String(i+1)+'!</font></small><br>';
+        entity.innerHTML+='&nbsp;&nbsp;&nbsp;<label>Price</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Price'+i+'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small id="" class="text-muted"><font color="blue">Enter Price of Entity '+String(i+1)+'!</font></small><br>';
+        entity.innerHTML+='&nbsp;&nbsp;&nbsp;<label>Volume</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Volume'+i+'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small id="" class="text-muted"><font color="blue">Enter Volume of Entity '+String(i+1)+'!</font></small><br><br>';
     }
 }
 
@@ -13,7 +14,8 @@ function loadUsers(){
     var n_users=document.getElementById('n_users').value;
     var i=0;
     var user=document.getElementById('user');
+    user.innerHTML='<h3>&nbsp;&nbsp;&nbsp;Player Information</h3><br><br>'
     for(;i<n_users;i++){
-        user.innerHTML+='<label>Seed</label><input type="number" name="seed'+i+'" required><br>';
+        user.innerHTML+='&nbsp;&nbsp;&nbsp;<label>Seed</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="number" name="seed'+i+'" required>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small id="" class="text-muted"><font color="blue">Enter Seed investment of Player '+String(i+1)+'!</font></small><br><br>';
     }
 }
